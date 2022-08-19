1f5d2d93da4e2fe5784d046df714a20e1678a0a0
Tommy <tommyphan2004.tp@gmail.com>
2022-08-19 12:46:41 +0200

    Fixed connection types with BE.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { test } from 'types';
 
 import './App.css';
 
 export const App = () => {
+    const x: test = 3;
+
     return (
         <h1>Test</h1>
     );
