cbb71e7d5e2205b427e0a19e4ad93fef53e8f5dc
Tommy Phan <tommyphan2004.tp@gmail.com>
2022-09-27 21:28:41 +0200

    Edited register view.

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,3 +1,4 @@
 import React from 'react';
+import { RegisterForm } from '../components/layout/RegisterFrom/RegisterForm';
 
-export const RegisterView = () => null;
+export const RegisterView = () => <RegisterForm/>;
