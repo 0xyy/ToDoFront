d4a2c437d13116126553bdea620e7457eda1b35f
Tommy <tommyphan2004.tp@gmail.com>
2022-11-28 20:59:18 +0100

    Fixed styling in not found page.

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavigationLink } from '../UI/NavigationLink';
 
 import notFound from '../../assets/not-found.svg';
 import styles from './NotFound.module.css';
@@ -7,7 +7,7 @@ import styles from './NotFound.module.css';
 export const NotFound = () => {
     return <div className={`${styles.box} card`}>
         <h1>Are you lost?</h1>
-        <p>Don't worry we will help you <Link to='/'>return to home page</Link></p>
+        <p>Don't worry we will help you <NavigationLink text='return to home page' to='/'/></p>
         <img src={notFound} alt="Not found image"/>
     </div>;
 };
